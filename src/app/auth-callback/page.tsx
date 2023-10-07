3cf24fd430fe6c809cb5dc339466c2a4f7dbd5e0
Shourya742 <sshourya17@gmail.com>
2023-10-07 13:05:00 +0530

    Added setup for next13, trpc, and structured the project along with front page

---
+++ src/app/auth-callback/page.tsx
@@ -0,0 +1,17 @@
+import { useSearchParams } from "next/navigation";
+import { useRouter } from "next/router";
+import { trpc } from "../_trpc/client";
+
+const Page = () => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const origin = searchParams.get("origin");
+  const { data, isLoading } = trpc.authCallback.useQuery(undefined, {
+    onSuccess: ({ success }) => {
+      if (success) {
+        router.push(origin ? `/${origin}` : "/dashboard");
+      }
+    },
+  });
+};
+export default Page;
