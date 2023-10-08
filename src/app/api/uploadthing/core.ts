ad2cd79609d7b9540d94b150645f13acc9617c12
Shourya742 <sshourya17@gmail.com>
2023-10-08 10:19:55 +0530

    Added ai message streaming

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -2,6 +2,9 @@ import { db } from "@/db";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { PDFLoader } from "langchain/document_loaders/fs/pdf";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import { getPineconeClient } from "@/lib/pinecone";
+import { PineconeStore } from "langchain/vectorstores/pinecone";
 const f = createUploadthing();
 
 export const ourFileRouter = {
@@ -24,7 +27,41 @@ export const ourFileRouter = {
       });
 
       try {
-      } catch (error) {}
+        const response = await fetch(
+          `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`
+        );
+        const blob = await response.blob();
+        const loader = new PDFLoader(blob);
+        const pageLevelDocs = await loader.load();
+        const pagesAmt = pageLevelDocs.length;
+        const pinecone = await getPineconeClient();
+        const pineconeIndex = pinecone.Index("pdfreader");
+        const embeddings = new OpenAIEmbeddings({
+          openAIApiKey: process.env.OPENAI_API_KEY,
+        });
+        await PineconeStore.fromDocuments(pageLevelDocs, embeddings, {
+          //@ts-ignore
+          pineconeIndex,
+          namespace: createdFile.id,
+        });
+        await db.file.update({
+          data: {
+            uploadStatus: "SUCCESS",
+          },
+          where: {
+            id: createdFile.id,
+          },
+        });
+      } catch (error) {
+        await db.file.update({
+          data: {
+            uploadStatus: "FAILED",
+          },
+          where: {
+            id: createdFile.id,
+          },
+        });
+      }
     }),
 } satisfies FileRouter;
 
--- src/lib/pinecone.ts
+++ src/lib/pinecone.ts
@@ -1,6 +1,9 @@
-import { Pinecone } from "@pinecone-database/pinecone";
-
-export const pinecone = new Pinecone({
-  apiKey: process.env.PINECONE_API_KEY!,
-  environment: "gcp-starter",
-});
+import { Pinecone, PineconeClient } from "@pinecone-database/pinecone";
+export const getPineconeClient = async () => {
+  const client = new PineconeClient();
+  await client.init({
+    apiKey: process.env.PINECONE_API_KEY!,
+    environment: "gcp-starter",
+  });
+  return client;
+};
